a82bde66ca316e8d1bed202a7126e3ac44463019
maqunchao <ma763485139@163.com>
2022-06-29 01:32:12 +0800

    money的第一栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import Tags from 'views/Tags';
-import Money from 'views/Money';
-import Statistics from 'views/Statistics';
+import Tags from "views/Tags";
+import Money from "views/Money";
+import Statistics from "views/Statistics";
 import NoMathch from "views/NoMathch";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/tags" element={<Tags />} />
-        <Route path="/money" element={<Money />} />
-        <Route path="/statistics" element={<Statistics />} />
-        <Route path="/" element={<Navigate to="/money" replace />} />
-        {/* 👇️ only match this when no other routes match */}
-        <Route
-          path="*"
-          element={<NoMathch />}
-        />
-      </Routes>
-    </HashRouter>
+    <AppWrapper>
+      <HashRouter>
+        <Routes>
+          <Route path="/tags" element={<Tags />} />
+          <Route path="/money" element={<Money />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/" element={<Navigate to="/money" replace />} />
+          {/* 👇️ only match this when no other routes match */}
+          <Route path="*" element={<NoMathch />} />
+        </Routes>
+      </HashRouter>
+    </AppWrapper>
   );
 }
-
-
 
 export default App;
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,16 +4,32 @@ import styled from "styled-components";
 
 const TagsSection = styled.section`
   background: #ffffff;
+  padding: 12px 16px;
+  > ol {
+    margin: 0 -12px;
+    > li {
+      background: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
 `;
 const NoteSection = styled.section``;
 
 const CategorySection = styled.section``;
 
 const NumberPadSection = styled.section``;
-const TagList = styled.ol`
-  > li {
-  }
-`;
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -24,12 +40,12 @@ function Money() {
   return (
     <MyLayout>
       <TagsSection>
-        <TagList>
+        <ol>
           <li>衣</li>
           <li>食</li>
           <li>住</li>
           <li>行</li>
-        </TagList>
+        </ol>
         <button>新增标签</button>
       </TagsSection>
       <NoteSection>
