c7fc7f7dce921dff34f909d21f61ca17382a9116
maqunchao <ma763485139@163.com>
2022-07-13 14:22:16 +0800

    tag页面样式提交

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const useTags = () => {
-  const [tags, setTags] = useState<string[]>(["衣", "食", "住"]);
+  const [tags, setTags] = useState<string[]>(["衣", "食", "住", "行"]);
   //需要导出一个对象
   return {
     tags,
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,19 +1,66 @@
 import React from "react";
 import Layout from "components/Layout";
 import { useTags } from "../useTags";
+import styled from "styled-components";
+import Icon from "components/Icon";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #767676;
+  border-radius: 4px;
+  color: white;
+`;
+
+const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Space = styled.div`
+  height: 16px;
+`;
 
 function Tags() {
   const { tags, setTags } = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {/* {tags.map((tag) => (
           <li key={tag}>{tag}</li>
         ))} */}
         {tags.map((tag) => {
-          return <li key={tag}>{tag}</li>;
+          return (
+            <li key={tag}>
+              <span className="oneLine">{tag}</span>
+              <Icon name="right" />
+            </li>
+          );
         })}
-      </ol>
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
